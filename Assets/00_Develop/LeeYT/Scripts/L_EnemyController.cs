575aaad4e397ec0ae7c37e70d1731519e0556275
Choc1015 <lytyys@naver.com>
2024-12-23 19:00:21 +0900

    feat : follow Enemy System

---
+++ Assets/00_Develop/LeeYT/Scripts/L_EnemyController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+public class L_EnemyController : Human
+{
+    private GameObject Player;
+    private Vector3 moveDir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        statController.Init();
+        FindPlayers();
+    }
+
+    private void FindPlayers()
+    {
+        GameObject[] Players;
+        if ( (Players = GameObject.FindGameObjectsWithTag("Player")) == null)
+                return;
+
+        int playerIndex = Random.Range(0, Players.Length);
+        Player = Players[playerIndex];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Player != null) 
+        FollowPlayer();
+    }
+
+    void FollowPlayer()
+    {
+        moveDir = Player.transform.position - transform.position;
+
+        if(moveDir != null)
+        movement.MoveTo(moveDir, statController.GetStat(StatInfo.MoveSpeed).Value);
+    }
+
+
+}
