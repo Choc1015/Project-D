99a6016da55d9ed836c757bc6a311361b5580b95
Choc1015 <98998206+Choc1015@users.noreply.github.com>
2024-12-28 23:04:56 +0900

    feat : EnemySpawnBox

---
+++ Assets/00_Develop/LeeYT/Scripts/EnemyManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : Singleton<EnemyManager>
+{
+
+    public EnemyStateMachine[] EnemyPrefab; // 총알 프리팹
+    public int initialPoolSize = 10; // 초기 풀 크기
+    private Dictionary<EnemyStateMachine, ObjectPool<EnemyStateMachine>> enemyPools = new(); // 적 프리팹별 객체 풀
+
+    private void Start()
+    {
+        foreach (var enemy in EnemyPrefab)
+        {
+            enemyPools[enemy] = new ObjectPool<EnemyStateMachine>(enemy, initialPoolSize, transform);
+        }
+    }
+
+    public EnemyStateMachine SpawnEnemy(EnemyStateMachine enemyPrefab, Vector3 position)
+    {
+        enemyPrefab = enemyPools[enemyPrefab].SpawnObject();
+        enemyPrefab.transform.position = position;
+        return null;
+    }
+
+    public void ReturnEnemy(EnemyStateMachine enemyPrefab, EnemyStateMachine enemy)
+    {
+        // 적 객체를 풀로 반환
+        if (enemyPools.TryGetValue(enemyPrefab, out var pool))
+        {
+            pool.DespawnObject(enemy);
+        }
+        else
+        {
+            Debug.LogError("Enemy prefab not found in pool.");
+        }
+    }
+
+
+
+}
