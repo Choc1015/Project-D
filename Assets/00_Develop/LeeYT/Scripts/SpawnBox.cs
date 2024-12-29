472282206d30fea7e82e7b1a82bcc4cab7e80b7b
Choc1015 <98998206+Choc1015@users.noreply.github.com>
2024-12-29 19:59:52 +0900

    fix : Change ObjectPoolManager

--- Assets/00_Develop/LeeYT/Scripts/SpawnBox.cs
+++ Assets/00_Develop/LeeYT/Scripts/SpawnBox.cs
@@ -5,12 +5,12 @@ using UnityEngine.UI;
 
 public class SpawnBox : MonoBehaviour
 {
-    public EnemyStateMachine Prefab = new(); // 积己且 橇府普
+    public GameObject Prefab; // 积己且 橇府普
 
     private void Start()
     {
         // 利 积己
-        var enemy = EnemyManager.Instance.SpawnEnemy(Prefab, transform.position);
+        var enemy = ObjectPoolManager.Instance.SpawnFromPool(Prefab.name, transform.position);
         gameObject.SetActive(false);
         Debug.Log("利 积己");
 
