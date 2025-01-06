34c13a19ffd6b81c73af51c8de4cc92d7db5c5cf
77Sun77 <84917607+77Sun77@users.noreply.github.com>
2025-01-06 17:28:27 +0900

    feat : Statue Image

--- Assets/00_Develop/Sun/Scripts/Statue.cs
+++ Assets/00_Develop/Sun/Scripts/Statue.cs
@@ -8,6 +8,9 @@ public class Statue : MonoBehaviour
     private RaycastHit2D hit;
     private int layerMask;
     public PlayerType playerType;
+
+    public SpriteRenderer sprite;
+    public Sprite[] sprites;
     void Update()
     {
         
@@ -16,9 +19,12 @@ public class Statue : MonoBehaviour
             if (layerMask == default)
                 layerMask = 1 << LayerMask.NameToLayer("Player");
 
-            hit = Physics2D.BoxCast(transform.position, Vector2.one, 0, Vector2.zero, 0, layerMask);
+            hit = Physics2D.BoxCast(transform.position, Vector2.one*3, 0, Vector2.zero, 0, layerMask);
             Utility.GetPlayer().OnTriggerStatue(hit, playerType);
+            sprite.sprite = hit ? sprites[1] : sprites[0];
         }
+        else if(sprite.sprite == sprites[1])
+            sprite.sprite = sprites[0];
     }
 }
 public class ReviveInfo
